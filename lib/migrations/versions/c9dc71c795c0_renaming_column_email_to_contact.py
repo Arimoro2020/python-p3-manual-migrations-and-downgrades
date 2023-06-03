98d2909233eab2f3dbfbae1403eb12c61a34a593
Adeyemi Arimoro <arimoro71@gmail.com>
2023-06-03 07:00:18 -0500

    Add solution

---
+++ lib/migrations/versions/c9dc71c795c0_renaming_column_email_to_contact.py
@@ -0,0 +1,24 @@
+"""Renaming column email to contact
+
+Revision ID: c9dc71c795c0
+Revises: 791279dd0760
+Create Date: 2023-06-03 06:43:33.849663
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c9dc71c795c0'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+   pass
